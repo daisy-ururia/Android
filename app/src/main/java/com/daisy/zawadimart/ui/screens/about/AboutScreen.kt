aab920ee1fc85ca0181a9e4e04bdaca0e07a3ecf
daisy-ururia <uluriadaisy@gmail.com>
2025-04-01 15:54:08 +0300

    initial commit

---
+++ app/src/main/java/com/daisy/zawadimart/ui/screens/about/AboutScreen.kt
@@ -0,0 +1,15 @@
+package com.daisy.zawadimart.ui.screens.about
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun AboutScreen(){
+
+}
+
+@Preview
+@Composable
+fun AboutScreenPreview(){
+
+}
